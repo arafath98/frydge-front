460f550c7374ed4b4e671053141b5186a7777fab
mussb00 <berhanem2000@gmail.com>
2021-11-13 18:07:22 +0000

    navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import NavigationBar from "../../components/UI/NavigationBar";
 export default function Home() {
     return (
-        <div>
-            <h1>HOME PAGE</h1>
-        </div>
-        
+        <>
+            <NavigationBar />
+            <div class="container mt-4">
+                <h1>Tired of wasting food and money?</h1>
+                <p>Our app enables you to track your fridge to ensure you never have to throw away goods again.</p>
+            </div>
+        </>
+
     );
 };
