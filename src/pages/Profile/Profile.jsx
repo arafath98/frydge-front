62ebf463fad2317c142550681caa1c7a7e19137f
mussb00 <berhanem2000@gmail.com>
2021-11-15 18:26:11 +0000

    have to be logged in to access private routes

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,19 @@
-// import { useContext } from "react";
-
-// import { Context } from "../../Context";
-
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Context } from "../../Context";
 import AddEmail from "../../components/Email/AddEmail";
 
 export default function Profile() {
     document.title = 'Profile';
-
+    const { isLoggedIn } = useContext(Context)
+    const navigate = useNavigate()
     // const { colors, theme } = useContext(Context);
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            navigate("/login")
+        }
+    }, [])
 
     const addEmail = (email) => {
         console.log(`${email} has been successfully added.`);
