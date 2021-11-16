72f6b4abf5489375ea5ec42a19469c78e06834dc
devSamC <samuelcockroft@outlook.com>
2021-11-16 16:41:16 +0000

    added more ridirects into the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,8 +6,13 @@ export default function Footer() {
     const navigate = useNavigate()
     
     const handleItem = () => {
-        navigate('/home')
-        console.log('clicked')
+        navigate('/home')   
+    }
+    const handleList = () => {
+        navigate('/list')
+    }
+    const handleProfile = () => {
+        navigate('/profile')
     }
     return (
         <div>
@@ -15,8 +20,8 @@ export default function Footer() {
                 <div className="footerButtons">
                 <a onClick={handleItem}>ITEMS</a>
                 <a>ADD ITEM</a>
-                <a>SHOPPING LIST</a>
-                <a>PROFILE</a>
+                <a onClick={handleList}>SHOPPING LIST</a>
+                <a onClick={handleProfile}>PROFILE</a>
                 </div>
             </footer>
             
