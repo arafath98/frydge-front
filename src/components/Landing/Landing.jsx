fee2ed4e59ad030bb75b8cbcb4d2b5271d87b5a1
MathusanKandiah <mathusankandiah@hotmail.com>
2021-11-17 12:10:02 +0000

    adding iamges

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -7,13 +7,14 @@ const Landing = () => {
 
             <h1 className="display-4">Welcome to Frydge!</h1>
         <hr />
-            <p className="lead">
-                Frydge is your personal online fridge, keeping track of your perishable items, notifying you when
-                they are close to their expiry and in turn saving you money and most of all, being part of the 
+            <p className="display-6 lead">
+                Frydge is your personal online fridge. <br/> 
+                Keeping track of your perishable items. Get notified when
+                they are close to their expiry. Be part of the 
                 global effort to reduce food waste.
 
             </p>
-            <img src="https://i.ibb.co/NKY6B3S/wastage.png" alt="Foodwastage" />
+            <img src="./images/123.png" alt="Foodwastage" />
         </div>
         </>
     )
